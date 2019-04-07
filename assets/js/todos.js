537d8184e9f8aa9844412c2aa54b65dbb4df2bb4
Neal Miran <neal.miran@gmail.com>
2019-04-06 22:27:04 -0400

    Initial commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,18 @@
-alert("connected")
+//Check off specific Todos by Clicking
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to Delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        console.log("you hit enter");
+    }
+})
